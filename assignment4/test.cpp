d2e5f595e6df807101aa54e7d65e8f7b89ae1483
Klementina Stojanovska <stojankz@miamioh.edu>
2014-11-07 17:36:44 -0500

    add comments to the test file

--- assignment4/test.cpp
+++ assignment4/test.cpp
@@ -13,6 +13,7 @@ using namespace std;
 
 using namespace ods;
 
+//Method to add values to the BinarySearchTree
 void addValuesInTree(BinarySearchTree<BSTNode1<int>,int>& t){
 	cout<<"Adding the values 4, 2, 7, 1, 3, 9 to the "
 			"BinarySearchTree, in that order." <<endl;
@@ -24,22 +25,30 @@ void addValuesInTree(BinarySearchTree<BSTNode1<int>,int>& t){
 	t.add(9);
 }
 
+//Method that prints the size of the tree
 void getSizeOfTree(BinarySearchTree<BSTNode1<int>,int>& t){
 	int size = t.size();
 	cout<<"The size of the tree is: " <<size<<endl;
 }
 
+//Method that tests the height2() method and
+//prints the height of the tree
 void testHeight2(BinarySearchTree<BSTNode1<int>,int>& t){
 	int height = t.height2();
 	cout<<"The height of the tree is: "<<height<<endl;
 }
 
+//Method that tests the isBalanced() method and
+//prints the result
 void testIsBalanced(BinarySearchTree<BSTNode1<int>,int>& t){
 	bool balanced = t.isBalanced();
 	cout<<"Is this tree balanced?(If yes, returns 1, if not returns 0): "<<
 			balanced<<endl;
 }
 
+//Method that runs the preOrderNumber() method and
+//then prints the pre-order numbers for all the
+//elements in the tree.
 void testPreOrderNumber(BinarySearchTree<BSTNode1<int>,int>& t){
 	cout<<"Assigning pre-order numbers to the tree."<<endl;
 	t.preOrderNumber();
@@ -51,6 +60,9 @@ void testPreOrderNumber(BinarySearchTree<BSTNode1<int>,int>& t){
 	cout<<"The pre-order number for 9 is: " << t.getNode(9)->pre_number<<endl;
 }
 
+//Method that runs the inOrderNumber() method and
+//then prints the in-order numbers for all the
+//elements in the tree.
 void testInOrderNumber(BinarySearchTree<BSTNode1<int>,int>& t){
 	cout<<"Assigning in-order numbers to the tree."<<endl;
 	t.inOrderNumber();
@@ -62,15 +74,9 @@ void testInOrderNumber(BinarySearchTree<BSTNode1<int>,int>& t){
 	cout<<"The in-order number for 9 is: " << t.getNode(9)->in_number<<endl;
 }
 
-void testGetLE(BinarySearchTree<BSTNode1<int>,int>& t, int i){
-	cout<<"The list of numbers less than or equal to "<<i<< " are: "<<endl;
-	DLList<int> list = t.getLE(i);
-	for(int i = 0; i< list.size(); i++){
-		cout<< list.get(i) << " ";
-	}
-	cout<<""<<endl;
-}
-
+//Method that runs the postOrderNumber() method and
+//then prints the post-order numbers for all the
+//elements in the tree.
 void testPostOrderNumber(BinarySearchTree<BSTNode1<int>,int>& t){
 	cout<<"Assigning post-order numbers to the tree."<<endl;
 	t.postOrderNumber();
@@ -82,6 +88,18 @@ void testPostOrderNumber(BinarySearchTree<BSTNode1<int>,int>& t){
 	cout<<"The post-order number for 9 is: " << t.getNode(9)->post_number<<endl;
 }
 
+//Method that tests the getLE(i) method and then prints
+//all the values that are less than or equal to i.
+void testGetLE(BinarySearchTree<BSTNode1<int>,int>& t, int i){
+	cout<<"The list of numbers less than or equal to "<<i<< " are: "<<endl;
+	DLList<int> list = t.getLE(i);
+	for(int i = 0; i< list.size(); i++){
+		cout<< list.get(i) << " ";
+	}
+	cout<<""<<endl;
+}
+
+//The main method calls all of the methods.
 int main() {
 	BinarySearchTree<BSTNode1<int>, int > tree;
 	addValuesInTree(tree);
